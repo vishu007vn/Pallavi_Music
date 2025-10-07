94203680e3f40df7c375f9bdf68ad968d87c433e
Vishulover680 <vishal255g@gmail.com>
2025-10-07 06:53:57 +0530

    Update README.m
    Signed-off-by: Vishulover680  <vishal255g@gmail.com>

--- README.m
+++ README.m
@@ -5,20 +5,8 @@
  
 ### 🚩 जय महाकाल 🚩
 <h1 align="center"
-  
-<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
-<img src="https://readme-typing-svg.herokuapp.com?color=FF0085&width=620&lines=🍁+🚩+ᴘᴏᴡᴇʀᴇᴅ+ʙʏ+ᴧᴧʀᴜᴍɪ+ʙᴏᴛs+🚩+🍁"></b></h3>
-<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
-<h1 align="center"><b>ᴛᴇᴧᴍ ᴧᴧʀᴜᴍɪ ʙᴏᴛs</b></h1>
-<p align="center"><a href="https://t.me/DevilRajput07"><img src="https://files.catbox.moe/783eei.jpg" width="400"></a></p>
-<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
-
 ━━━━━━━━━━━━━━━━━━━━
-
-
-
-
 ### Contact :
-<a href="https://t.me/DevilRajput07"><img title="Telegram" src="https://img.shields.io/badge/Telegram-%23000000.svg?&style=for-the-badge&logo=telegram&logoColor=61DAFB"></a>
+<a href="https://t.me/M4fia007"><img title="Telegram" src="https://img.shields.io/badge/Telegram-%23000000.svg?&style=for-the-badge&logo=telegram&logoColor=61DAFB"></a>
 
 <a href="https://www.instagram.com/vishulover680"><img title="Instagram" src="https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white"></a>
